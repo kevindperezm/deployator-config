ef9fecf1fbd479705638dc45898aa78a60e85987
Kevin Perez <kevindperezm@gmail.com>
2014-08-04 15:16:33 -0500

    define new steps for changes in login feature

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,17 +1,21 @@
-Given /^I visit (.*)$/ do |url|
-  visit url
+Given /^I have visited the login page$/ do
+  visit google_apps_sign_in_path
 end
 
 Given /^I have entered (.*) as the user$/ do |user|
-  fill_in 'User', with: user
+  fill_in 'Email', with: user
 end
 
 Given /^I have entered (.*) as the password$/ do |password|
-  fill_in 'Password', with: user
+  fill_in 'Passwd', with: password
 end
 
-When /^I click on (.*)$/ do |clickable|
-  click_on clickable
+When /^I click the login button$/ do
+  click_on 'signIn'
+end
+
+When /^I approve access to my data$/ do
+  click_on 'submit_approve_access'
 end
 
 Then /^I should see the apps dashboard page$/ do
