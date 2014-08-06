d5651d4f46a9509e202a5a69ab50f173a404a7be
Kevin Perez <kevindperezm@gmail.com>
2014-08-06 11:37:47 -0500

    remove routes for deprecated crowdint_auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
   get 'apps' => 'apps#index'
   get 'bot_config' => 'bot_config#index'
 
-  devise_scope :user do
-    get 'google_apps_sign_in', to: "crowdint_auth/omniauth_callbacks#google_apps_sign_in"
-  end
-
-  devise_for :users, controllers: {omniauth_callbacks: 'crowdint_auth/omniauth_callbacks'}
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 end
