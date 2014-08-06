7bedc1545536ffbf0673e8b2a5c9a3ad91574626
Kevin Perez <kevindperezm@gmail.com>
2014-08-06 18:47:56 -0500

    remove unneeded fields in devise users migration

--- db/migrate/20140804180509_devise_create_users.rb
+++ db/migrate/20140804180509_devise_create_users.rb
@@ -4,13 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      ## Rememberable
-      t.datetime :remember_created_at
 
       t.timestamps
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
   end
 end
