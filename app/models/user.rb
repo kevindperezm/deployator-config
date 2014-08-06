329377b6c5a99b9a1ed1de4ac1d557f89f497552
Kevin Perez <kevindperezm@gmail.com>
2014-08-06 11:35:04 -0500

    rewrite find_or_create_for_google_oauth2 method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :omniauthable
+  devise :database_authenticatable, :rememberable, :omniauthable
+
+  def self.find_or_create_for_google_oauth2(data)
+    params = { email: data["email"] }
+    User.where(params).first || User.create!(
+      email: data['email'],
+      first_name: data['first_name'],
+      last_name: data['last_name'],
+      picture: data['image']
+    )
+  end
 end
