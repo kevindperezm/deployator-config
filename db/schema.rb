066eb34abce81bb958979593981a53e894d369fb
Kevin Perez <kevindperezm@gmail.com>
2014-08-06 19:30:38 -0500

    run pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@
 ActiveRecord::Schema.define(version: 20140806162805) do
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
