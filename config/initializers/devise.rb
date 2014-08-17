69ec716ab27e4d986593a57d5c51791f6a5beed9
Kevin Perez <kevindperezm@gmail.com>
2014-08-17 04:09:44 -0500

    let users select which Google account use to sign in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
-                  hd: 'crowdint.com'
+                  prompt: 'select_account', hd: 'crowdint.com'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
