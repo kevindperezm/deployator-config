d8b37f0e30e49b0ec9bfc52c5e0d3a8fec93b981
Kevin Perez <kevindperezm@gmail.com>
2014-08-18 23:12:38 -0500

    add feature spec for set slack and github tokens feature

--- features/step_definitions/define_steps.rb
+++ features/step_definitions/define_steps.rb
@@ -1,11 +1,31 @@
+Given /^I am at (.*) page$/ do |page|
+  case page
+  when 'the bot config page'
+    visit bot_config_path
+  end
+end
+
 When /^I sign in$/ do
   visit root_path
   click_on 'login'
 end
 
-Then /^I should see the (.*) page$/ do |page|
-  case page
-  when 'apps dashboard'
+When /^I put (.*) as (.*)$/ do |value, field|
+  field.gsub!(' ', '_').downcase!
+  fill_in field, with: value
+end
+
+When /^I click on (.*)$/ do |clickable|
+  click_on clickable
+end
+
+Then /^I should see (.*)$/ do |what|
+  case what
+  when 'the apps dashboard page'
     expect(current_path).to eql apps_path
+  when 'instructions of what to do with the command'
+    expect(page).to have_css '#command_instructions'
+  else
+    expect(page).to have_content(what)
   end
 end
