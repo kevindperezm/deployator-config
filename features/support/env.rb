b63e4d14471b6725624c8f25caefeab399692e44
Kevin Perez <kevindperezm@gmail.com>
2014-08-18 20:00:03 -0500

    add stub for login testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -28,6 +28,18 @@ require 'cucumber/rails'
 #
 ActionController::Base.allow_rescue = false
 
+# OmniAuth configuration for cucumber tests
+OmniAuth.configure do |config|
+  config.test_mode = true
+  config.add_mock :default, {
+    :info => {
+      :email => 'foobar@crowdint.com',
+      :name => 'foo',
+      :password => 'qwerty123'
+    }
+  }
+end
+
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
