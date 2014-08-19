fcc9d77b86a323204c2cf4310e2a4c2a834b9b65
Kevin Perez <kevindperezm@gmail.com>
2014-08-19 02:09:13 -0500

    add apart_from scope to BotConfig model

--- app/models/bot_config.rb
+++ app/models/bot_config.rb
@@ -2,4 +2,6 @@ class BotConfig < ActiveRecord::Base
   validates_presence_of :bot_name, :bot_url,
                         :slack_team, :slack_token,
                         :github_organization, :github_token
+
+  scope :apart_from, lambda { |config| where('id != ?', config.id) }
 end
