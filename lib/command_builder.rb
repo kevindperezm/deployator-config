6d45a13ae94b7a3ab8f687b328475665309ec448
Kevin Perez <kevindperezm@gmail.com>
2014-08-19 01:55:29 -0500

    add CommandBuilder class

---
+++ lib/command_builder.rb
@@ -0,0 +1,19 @@
+class CommandBuilder
+  attr_reader :config
+
+  def self.build_from(config)
+    new(config).build
+  end
+
+  def initialize(config)
+    @config = config
+  end
+
+  def build
+    "heroku config:set " <<
+    "HUBOT_BOT_NAME=#{@config.bot_name} " <<
+    "HUBOT_SLACK_TEAM=#{@config.slack_team} " <<
+    "HUBOT_SLACK_TOKEN=#{@config.slack_token} " <<
+    "HUBOT_GITHUB_TOKEN=#{@config.github_token}"
+  end
+end
