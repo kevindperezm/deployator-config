9ddac6e89bb5770776b82458730ac159f1cdf051
Kevin Perez <kevindperezm@gmail.com>
2014-08-18 20:01:07 -0500

    change login spec to use the stub

--- features/login.feature
+++ features/login.feature
@@ -3,10 +3,6 @@ Feature: Login
   I want to login to the app
   so I can start using the system
 
-  @javascript
   Scenario: Login with kevin.perez@crowdint.com account
-    Given I am at the home page
-    And I click the sign in button
     When I sign in
-    And I authorize the app
     Then I should see the apps dashboard page
--- features/step_definitions/define_steps.rb
+++ features/step_definitions/define_steps.rb
@@ -1,28 +1,6 @@
-Given /^I am at the (.*) page$/ do |page|
-  case page
-  when 'home'
-    visit root_path
-  end
-end
-
-Given /^I click the (.*) button$/ do |button|
-  case button
-  when 'sign in'
-    click_on 'login'
-  when 'login'
-    click_on 'signIn'
-  end
-end
-
 When /^I sign in$/ do
-  fill_in 'Email', with: 'kevin.perez@crowdint.com'
-  fill_in 'Passwd', with: 'crowdsmkd123?'
-  click_on 'signIn'
-end
-
-When /^I authorize the app$/ do
-  sleep 60
-  click_on 'Aceptar'
+  visit root_path
+  click_on 'login'
 end
 
 Then /^I should see the (.*) page$/ do |page|
