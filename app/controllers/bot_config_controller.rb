e8174b76fa8979ea1b42288a66205fa193870d42
Kevin Perez <kevindperezm@gmail.com>
2014-08-19 00:51:32 -0500

    add unfinished save action to bot config controller

--- app/controllers/bot_config_controller.rb
+++ app/controllers/bot_config_controller.rb
@@ -3,4 +3,26 @@ class BotConfigController < ApplicationController
 
   def index
   end
+
+  def save
+    @config = BotConfig.create(config_params)
+    if @config.save
+      delete_previous_config
+      render json: { command: generate_command_for(@config) }, status: :created
+    end
+  end
+
+  private
+
+  def delete_previous_config
+  end
+
+  def config_params
+    params.require('bot_config').permit :bot_url, :bot_name,
+                                        :slack_team, :slack_token,
+                                        :github_organization, :github_token
+  end
+
+  def generate_command_for(config)
+  end
 end
