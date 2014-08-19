4f64ba39421b71084d59670d2d972d23878b7b1b
Kevin Perez <kevindperezm@gmail.com>
2014-08-19 00:49:38 -0500

    create post route to bot_config#save

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'apps' => 'apps#index'
   get 'bot_config' => 'bot_config#index'
+  post 'bot_config' => 'bot_config#save', as: 'bot_config_save'
 
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 end
