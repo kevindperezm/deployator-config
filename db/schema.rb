cb679e4891aeaa8eead1775ac55fdd0777c78f8b
Kevin Perez <kevindperezm@gmail.com>
2014-08-20 19:25:45 -0500

    run pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819042548) do
+ActiveRecord::Schema.define(version: 20140821002353) do
 
   create_table "bot_configs", force: true do |t|
     t.string   "bot_url"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140819042548) do
     t.string   "github_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "deploy_timeout"
   end
 
   create_table "users", force: true do |t|
