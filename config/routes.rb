67ab9a4fea9111432870f6cc7803d078e87070b1
Kevin Perez <kevindperezm@gmail.com>
2014-08-24 15:49:38 -0500

    add resource route for apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get 'apps' => 'apps#index'
+  resources :apps
+
   get 'bot_config' => 'bot_config#show'
   post 'bot_config' => 'bot_config#save', as: 'bot_config_save'
 
