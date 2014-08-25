60f740b55c7373c855630b3faf143c2f60d30252
Kevin Perez <kevindperezm@gmail.com>
2014-08-25 17:51:10 -0500

    load apps when accessing the apps dashboard

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,5 +2,10 @@ class AppsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    begin
+      @apps = App.all
+    rescue SocketError
+      @apps = nil
+    end
   end
 end
