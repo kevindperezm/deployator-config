442826e1c8d5a8cfe3ebca215d2c5e894b0df860
Kevin Perez <kevindperezm@gmail.com>
2014-08-25 19:31:23 -0500

    protect against connection error and socket error when listing apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,10 +2,18 @@ class AppsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @apps = load_apps
+    @bot_connection_error = true unless @apps
+  end
+
+  private
+
+  def load_apps
     begin
-      @apps = App.all
-    rescue SocketError
-      @apps = nil
+      App.all
+    rescue ActiveResource::ConnectionError, SocketError
+      nil
     end
   end
+
 end
