47d30092d37392c9d5a585ccb459a206048ecdf6
Kevin Perez <kevindperezm@gmail.com>
2014-09-09 13:09:19 -0500

    throw RemoteBotIOError when can't set/get remote bot config

--- lib/remote_bot.rb
+++ lib/remote_bot.rb
@@ -10,11 +10,19 @@ class RemoteBot
 
   def config
     Unirest.get("#{@url}#{CONFIG_PATH}").body
+    rescue Exception
+      raise RemoteBotIOError
   end
 
   def config=(new_config)
-    headers = { 'Content-Type' => 'application/json' }
-    Unirest.post "#{@url}#{CONFIG_PATH}", headers: headers,
-                                          parameters: new_config.to_json
+    begin
+      headers = { 'Content-Type' => 'application/json' }
+      Unirest.post "#{@url}#{CONFIG_PATH}", headers: headers,
+                                            parameters: new_config.to_json
+    rescue Exception
+      raise RemoteBotIOError
+    end
   end
 end
+
+class RemoteBotIOError < IOError; end
